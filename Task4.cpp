e6e35616c3dcf986d95b1bf497b2e21d812679cb
InnaTarasyuk <60796644+InnaTarasyuk@users.noreply.github.com>
2020-11-30 01:53:48 +0300

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -3,86 +3,83 @@
 #include <chrono>
 #include <vector>
 #include <algorithm>
-using std::ref;
+
+
 using std::string;
 using std::vector;
-void threadFunction(vector<int> team, int l, int r) {
-	for (int i = l; i < r - 1;i += 2) {
-		if (team[i] < team[i + 1]) {
-			team[i] = -1;
-		}
-		else {
-			team[i + 1] = -1;
-		}
-	}
+
+void threadFunction(vector<int> &team, int l, int r) {
+    for (int i = l; i < r - 1; i += 2) {
+        if (team[i] < team[i + 1]) {
+            team[i] = 0;
+        } else {
+            team[i + 1] = 0;
+        }
+    }
 }
 
 int input() {
-	int n;
-	std::cin >> n;
-	bool test = true;
-	do {
-		if (!(test = std::cin.good())) {
-			std::cout << "Incorrect input. Try again." << std::endl;
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-	} while (!test);
-	return n;
+    int n;
+    std::cin >> n;
+    bool test = true;
+    do {
+        if (!(test = std::cin.good())) {
+            std::cout << "Incorrect input. Try again." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    } while (!test);
+    return n;
 }
 
 int main() {
-	int n;
-	int max_energy;
-	vector<int> team;
-	//vector<int> winners;
-	do {
-		std::cout << "Input a number of monks <= 100:" << std::endl;
-		n = input();
-	} while (n <= 0 || n > 100);
-	do {
-		std::cout << "Input max energy value: " << std::endl;
-		max_energy = input();
-	} while (max_energy <= 0);
+    int n;
+    int max_energy;
+    do {
+        std::cout << "Input a number of monks <= 100:" << std::endl;
+        n = input();
+    } while (n <= 0 || n > 100);
+    do {
+        std::cout << "Input max energy value: " << std::endl;
+        max_energy = input();
+    } while (max_energy <= 0);
 
+    vector<int> team;
+    for (size_t i = 0; i < n; ++i) {
+        team.push_back(rand() % max_energy + 1);
+        std::cout << "Monk " << i << " with energy " << team[i] << std::endl;
+    }
 
-	for (size_t i = 0; i < n; ++i) {
-		team.push_back(rand() % max_energy + 1);
-		std::cout << "Monk " << i << " with energy " << team[i] << std::endl;
-	}
-
-	while (n / 2 + n % 2 >= 2) {
+    while (n / 2 + n % 2 >= 2) {
 
 #pragma omp parallel
-		{
+        {
 #pragma omp sections
-			{
-#pragma omp section {
-				threadFunction(team, 0, n / 2);
-				}
-#pragma omp section{
-			threadFunction(team, n / 2, n);
-			}
-			}
-		}
-		int a = -1;
+            {
+#pragma omp section
+                {
+                    threadFunction(team, 0, n / 2);
+                }
+#pragma omp section
+                {
+                    threadFunction(team, n / 2, n);
+                }
 
-		team.erase(remove(team.begin(), team.end(), a), team.end());
-	
-		n = team.size();
+            }
+        }
 
-		/*	for (size_t i = 0; i < team.size(); i++)
-			{
-				std::cout << "\tMonk with energy " << team[i] << std::endl;
-			}*/
-	}
-	if (team[0] > team[1])
-	{
-		std::cout << "Win monk with energy " << team[0] << std::endl;
-	}
-	else {
-		std::cout << "Win monk with energy " << team[1] << std::endl;
-	}
+        int a = 0;
+        team.erase(std::remove(team.begin(), team.end(), a), team.end());
+        n = team.size();
 
-	return 0;
+    }
+    if (team[0] > team[1]) {
+        std::cout << "Win monk with energy " << team[0] <<
+                  std::endl;
+    } else {
+        std::cout << "Win monk with energy " << team[1] <<
+                  std::endl;
+    }
+
+    return 0;
 }
